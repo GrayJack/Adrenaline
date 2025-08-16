eda64c8d0a49b7a9b0de6e700843d7410662290d
GrayJack <gr41.j4ck@gmail.com>
2025-08-16 15:32:22 -0300

    feat(vshctrl): Squashed developed VSHControl features
    - feat(vshctrl): Add support to show DAX, JSO, ZSO, CSOv2 images on XMB
    - feat(vshctrl): Add support for manuals that require `DOCINFO.EDAT` files
    - refactor(vshctrl): Simplify isoreader
    
    For manual DOCUMENT.DAT:
    - Attempt first at /ISO/<iso-name>.DAT
    - Attempt second at /GAME/<GAME-ID>/DOCUMENT.DAT
    
    For manual DOCINFO.EDAT:
    - Attempt first at /ISO/<iso-name>.EDAT
    - Attempt second at /GAME/<GAME-ID>/DOCINFO.EDAT
    - Causes ENOENT error otherwise

---
+++ cef/vshctrl_new/main.c
@@ -0,0 +1,82 @@
+/*
+	Adrenaline
+	Copyright (C) 2016-2018, TheFloW
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stddef.h>
+#include <string.h>
+#include <strings.h>
+
+#include <pspctrl.h>
+#include <pspsysmem_kernel.h>
+#include <systemctrl.h>
+#include <systemctrl_se.h>
+
+#define _ADRENALINE_LOG_IMPL_
+#include <adrenaline_log.h>
+
+
+PSP_MODULE_INFO("VshControl", 0x1007, 2, 0);
+
+#define BOOT_BIN "disc0:/PSP_GAME/SYSDIR/BOOT.BIN"
+#define EBOOT_BIN "disc0:/PSP_GAME/SYSDIR/EBOOT.BIN"
+#define EBOOT_OLD "disc0:/PSP_GAME/SYSDIR/EBOOT.OLD"
+
+#define DUMMY_CAT_ISO_EXTENSION "     "
+
+#define MAX_FILES 128
+
+SceCtrlData *last_control_data = NULL;
+char mounted_iso[64];
+int has_umd_iso = 0;
+u32 psp_model = 0;
+
+AdrenalineConfig config;
+
+///////////////////////////////////////////////////////
+
+u32 firsttick;
+u8 set;
+
+SceUID modid = -1;
+
+int initPatch();
+
+int module_start(SceSize args, void *argp) {
+	logInit("ms0:/log_vshctrl.txt");
+	logmsg("VshCtrl started\n");
+
+	psp_model = sceKernelGetModel();
+	sctrlSEGetConfig(&config);
+
+	if (config.vsh_cpu_speed != 0) {
+		firsttick = sceKernelGetSystemTimeLow();
+	}
+
+	initPatch();
+
+	// always reset to NORMAL mode in VSH
+	// to avoid ISO mode is used in homebrews in next reboot
+	char* umdfile = sctrlSEGetUmdFile();
+	has_umd_iso = (umdfile[0] != 0 && sctrlSEGetBootConfFileIndex() == BOOT_VSHUMD);
+	if (has_umd_iso) strcpy(mounted_iso, umdfile);
+	sctrlSESetUmdFile("");
+	sctrlSESetBootConfFileIndex(BOOT_NORMAL);
+
+	logSync();
+
+	return 0;
+}
